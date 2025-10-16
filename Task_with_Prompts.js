6372fe42cbd91dc50bfa96c6af489c4d5f77fb70
Akholiwe-hub <akholiwenywebeni@gmail.com>
2025-10-16 10:56:27 +0200

    Task_with_Prompts

---
+++ Task_with_Prompts.js
@@ -0,0 +1,42 @@
+// let width = 20; 
+// let height = 10; 
+// let length = 50; 
+
+let your_Width = prompt("What is the width?"); 
+let your_Height = prompt("What is the height?"); 
+let your_Length = prompt("What is the length?");
+
+let volume = alert(your_Height * your_Length * your_Width);
+
+console.log(volume);
+
+
+
+
+
+
+// if (your_Width != width){
+//     console.log(alert("Not a number!"));
+// }  
+// else{
+//     console.log(alert("Great"));
+//     }
+
+
+// if (your_Height != height){
+//     console.log(alert("Not a number!"));
+// }  
+// else{
+//     console.log(alert("Great again!"));
+//     }
+
+// if (your_Length != length){
+//     console.log(alert("Not a number!"));
+// }  
+// else{
+//     console.log(alert("Great again even more!"));
+//     }
+
+// console.log(your_Height); 
+// console.log(your_Length); 
+// console.log(your_Width); 
